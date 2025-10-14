6a9be8d18f8c746535425f15cbaa9ef741ded86a
ghafarahmad635 <ghafarahmad635@gmail.com>
2025-10-14 16:38:30 +0500

    better auth

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,23 @@
+
+import { auth } from '@/lib/auth'
+import SignInView from '@/modules/auth/ui/views/sign-in-view'
+
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+
+const page = async() => {
+    const session = await auth.api.getSession({
+      headers: await headers() 
+  })
+  if(!!session){
+      redirect('/')
+    }
+  return (
+    <div>
+      <SignInView/>
+    </div>
+  )
+}
+
+export default page
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,58 @@
+'use client'
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client.ts";
+import { useState } from "react";
+
 
 export default function Home() {
+  const [name, setName] = useState("");
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
+const onSubmit=()=>{
+  authClient.signUp.email({
+    email:email,
+    name:name,
+    password:password
+  },{
+    onError:(ctx)=>{
+      alert("Error")
+    },
+    onSuccess:(ctx)=>{
+      alert("Success")
+    },
+     onRequest: (ctx) => {
+      alert("onRequest")
+     }
+  })
+}
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-     <Button>Test</Button>
+      <div>
+        <Input
+          placeholder="Name"
+          value={name}
+          type="text"
+          onChange={(e) => setName(e.target.value)} // ✅ correct
+        />
+        <Input
+          placeholder="Email"
+          value={email}
+          type="email"
+          onChange={(e) => setEmail(e.target.value)} // ✅ correct
+        />
+         <Input
+          placeholder="password"
+          value={password}
+          type="password"
+          onChange={(e) => setPassword(e.target.value)} // ✅ correct
+        />
+      </div>
+
+      <Button onClick={onSubmit}>
+        create User
+      </Button>
     </div>
   );
 }
